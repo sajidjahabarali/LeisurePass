7ab192fc61d4440a0d9b028c2d59f1ec4eaa0e36
Sajid Jahabar Ali <sajidjahabarali@gmail.com>
2021-08-08 14:31:26 +0100

    changed code to use ag-grid library

--- ui/src/components/ProductTable/ProductTable.jsx
+++ ui/src/components/ProductTable/ProductTable.jsx
@@ -1,71 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
 import axios from "axios";
 
-// https://www.ag-grid.com/react-grid/data-update-transactions/#example-updating-with-transaction
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
 export default function ProductTable() {
   const [rowData, setRowData] = useState(null);
-
+  const [colData, setColData] = useState(null);
   useEffect(() => {
     axios
       .get("http://localhost:5000/get-data")
       .then((response) => setRowData(response.data));
   }, []);
-
+  console.log(rowData);
   const classes = useStyles();
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">NAME</TableCell>
-            <TableCell align="right">DESCRIPTION</TableCell>
-            <TableCell align="right">CATEGORY_ID</TableCell>
-            <TableCell align="right">CREATION_DATE</TableCell>
-            <TableCell align="right">UPDATE_DATE</TableCell>
-            <TableCell align="right">LAST_PURCHASED_DATE</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rowData
-            ? rowData.map((row) => (
-                <TableRow key={row["ID"]}>
-                  <TableCell component="th" scope="row">
-                    {row["NAME"]}
-                  </TableCell>
-                  <TableCell align="right">{row["DESCRIPTION"]}</TableCell>
-                  <TableCell align="right">{row["CATEGORY_ID"]}</TableCell>
-                  <TableCell align="right">
-                    {row["LAST_PURCHASED_DATE"]}
-                  </TableCell>
-                  <TableCell align="right">{row["CREATION_DATE"]}</TableCell>
-                  <TableCell align="right">{row["UPDATE_DATE"]}</TableCell>
-                  <TableCell align="right">
-                    {row["LAST_PURCHASED_DATE"]}
-                  </TableCell>
-                </TableRow>
-              ))
-            : []}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData}>
+        <AgGridColumn field="ID" sortable={true} filter={true}></AgGridColumn>
+        <AgGridColumn field="NAME" sortable={true} filter={true}></AgGridColumn>
+        <AgGridColumn
+          field="DESCRIPTION"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="CATEGORY_ID"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="CREATION_DATE"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="UPDATE_DATE"
+          sortable={true}
+          filter={true}
+        ></AgGridColumn>
+        <AgGridColumn
+          field="LAST_PURCHASED_DATE"
+          sortable={true}
+        ></AgGridColumn>
+      </AgGridReact>
+    </div>
   );
 }
--- ui/src/components/TableCommands/TableCommands.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class TableCommands extends Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default TableCommands;
