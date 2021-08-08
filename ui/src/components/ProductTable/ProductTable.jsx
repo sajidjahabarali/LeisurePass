d99a16e6f351be703903f779d56ff5f86850f912
Sajid Jahabar Ali <sajidjahabarali@gmail.com>
2021-08-08 15:32:15 +0100

    added pagination

--- ui/src/components/ProductTable/ProductTable.jsx
+++ ui/src/components/ProductTable/ProductTable.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "./ProductTable.css";
 
 import axios from "axios";
 
@@ -14,17 +15,56 @@ const useStyles = makeStyles({
 
 export default function ProductTable() {
   const [rowData, setRowData] = useState(null);
-  const [colData, setColData] = useState(null);
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+
+    const updateData = (data) => {
+      setRowData(data);
+      // params.api.paginationGoToPage(0);
+    };
+
+    fetch("http://localhost:5000/get-data")
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
   useEffect(() => {
     axios
       .get("http://localhost:5000/get-data")
       .then((response) => setRowData(response.data));
   }, []);
-  console.log(rowData);
+
+  const onPageSizeChanged = (newPageSize) => {
+    var value = document.getElementById("page-size").value;
+    gridApi.paginationSetPageSize(Number(value));
+  };
+
   const classes = useStyles();
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-      <AgGridReact rowData={rowData}>
+    <div className="ag-theme-alpine grid">
+      <div className="example-header">
+        Page Size:
+        <select onChange={() => onPageSizeChanged()} id="page-size">
+          <option value="5" selected={true}>
+            5
+          </option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </div>
+      <AgGridReact
+        rowData={rowData}
+        pagination={true}
+        paginationPageSize={5}
+        paginationNumberFormatter={function (params) {
+          return "[" + params.value.toLocaleString() + "]";
+        }}
+        onGridReady={onGridReady}
+      >
         <AgGridColumn field="ID" sortable={true} filter={true}></AgGridColumn>
         <AgGridColumn field="NAME" sortable={true} filter={true}></AgGridColumn>
         <AgGridColumn
